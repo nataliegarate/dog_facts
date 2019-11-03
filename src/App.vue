4f9fec97300d2f793b6f6155461fbe73b517cf92
nataliegarate <natalieegarate@hotmail.com>
2019-11-03 14:32:08 -0500

    remove deleted medium and hard components because only single component is now needed

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,14 @@
-
-
 <script src="https://unpkg.com/vue/dist/vue.js"></script>
 <script src="https://unpkg.com/vue-router/dist/vue-router.js"></script>
 
 <template>
   <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <router-link to='/' id='heading'> <h1>
       <img height = '20' src = 'https://upload.wikimedia.org/wikipedia/commons/1/12/Black_Paw.svg'> Name that Dog! <img height = '20' src = 'https://upload.wikimedia.org/wikipedia/commons/1/12/Black_Paw.svg'>
 
       </h1></router-link>
 
     <div id='links'>
-    <!-- <router-link to='/'>Home</router-link> -->
-    <!-- <router-link to='/easy'>Easy</router-link>
-    <router-link to='/medium'>Medium</router-link>
-    <router-link to='/hard'>Hard</router-link> -->
-    <!-- <router-link to='/login'>Login</router-link> -->
-
 
 
     </div>
@@ -27,12 +18,9 @@
 
 <script>
 import Easy from './components/Easy.vue'
-import Medium from './components/Medium.vue'
-import Hard from './components/Hard.vue'
 import Router from 'vue-router'
 import Vue from 'vue'
 import router from './router.js'
-import Login from './components/Login.vue'
 import Home from './components/Home.vue'
 
 Vue.use(Router)
